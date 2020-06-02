55b46650af5d94c4eac0d2f7ae923c1fb2abf871
8 <lucinda.souchet@outlook.com>
2020-06-02 23:20:26 +0100

    update messages

--- run.py
+++ run.py
@@ -2,17 +2,26 @@ import os
 from flask import Flask
 
 app = Flask(__name__)
+messages = []
+
+def add_message(username, message):
+    message.append("{}: {}".format(username, message))
+
 
 @app.route('/')
 def index():
     """Main page with instructions"""
     return "To send a message use /USERNAME/MESSAGE"
 
+
 @app.route('/<username>')
 def user(username):
-    return "Hi " + username
+    """ Display chat messages """
+    return "Welcome, {0}".format(username, messages)
+
 
 @app.route('/<username>/<message>')
 def send_message(username, message):
+    """ Create a new message and redirect back to the chat page"""
     return "{0}: {1}".format(username, message)
 app.run(host=os.getenv('IP'), port=os.getenv('PORT'), debug=True)
